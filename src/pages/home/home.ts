141292e8888c06e47fc4bdf0175a14ea45060e54
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2017-05-23 19:37:35 -0400

    :tada: Data Binding

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,12 +8,35 @@ import { Person } from './person';
 })
 export class HomePage {
 
+  private persons: Person[] = [];
+  twitter: string = '@nicobytes'; 
+
   constructor(public navCtrl: NavController) {
+
     let person1 = new Person('nicolas',1);
-    console.log(person1.getAge());
-    console.log(person1.getName());
-    let person2 = new Person('true',22);
-    console.log(person2.getAge());
+    console.log('person1:Age',person1.getAge());//1
+    person1.setAge(23);
+    console.log('person1:Age',person1.getAge());//23
+    person1.setAge(2);
+    console.log('person1:Age',person1.getAge());//23
+    console.log('person1:Name',person1.getName());//nicolas
+    console.log('person1:Single',person1.getIsSingle());//true
+    let person2 = new Person('true',22, false);
+    console.log('person2:Age',person2.getAge());//true
+    console.log('person2:Name',person2.getName());//22
+    console.log('person2:Single',person2.getIsSingle());//false
+
+    this.persons.push(person1);
+    this.persons.push(person2);
+    console.log(this.persons);
+  }
+
+  showAlert(name: string){
+    alert('Hola '+ name);
+  }
+
+  updateTwitter( event ){
+    this.twitter = event.target.value;
   }
 
 }
--- src/pages/home/person.ts
+++ src/pages/home/person.ts
@@ -1,12 +1,10 @@
 export class Person{
-
-  name: string;
-  age: number;
-
-  constructor(name:string, age:number){
-    this.name = name;
-    this.age = age;
-  }
+  
+  constructor(
+    private name:string,
+    private age:number,
+    private isSingle: boolean = true
+  ){}
 
   getAge(){
     return this.age;
@@ -14,6 +12,21 @@ export class Person{
 
   getName(){
     return this.name;
+  }
+
+  getIsSingle(){
+    return this.isSingle;
+  }
+
+  setAge(age: number){
+    if(age >= 18){
+      this.age = age;
+      this.getState();
+    }
+  }
+
+  private getState(){
+    return 'Hola';
   }
 
 }
