c9515d723b112d0536d873c8f95dc09f53340dd5
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2017-05-23 20:56:18 -0400

    :tada: add and remove persons

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,7 +8,7 @@ import { Person } from './person';
 })
 export class HomePage {
 
-  private persons: Person[] = [];
+  persons: Person[] = [];
   twitter: string = '@nicobytes'; 
 
   constructor(public navCtrl: NavController) {
@@ -21,13 +21,16 @@ export class HomePage {
     console.log('person1:Age',person1.getAge());//23
     console.log('person1:Name',person1.getName());//nicolas
     console.log('person1:Single',person1.getIsSingle());//true
-    let person2 = new Person('true',22, false);
+    let person2 = new Person('andrea',22, false);
     console.log('person2:Age',person2.getAge());//true
     console.log('person2:Name',person2.getName());//22
     console.log('person2:Single',person2.getIsSingle());//false
 
     this.persons.push(person1);
     this.persons.push(person2);
+    this.persons.push(new Person('Nicolas',45, false));
+    this.persons.push(new Person('Marcela',34, true));
+    this.persons.push(new Person('vladimir',64, true));
     console.log(this.persons);
   }
 
@@ -35,8 +38,19 @@ export class HomePage {
     alert('Hola '+ name);
   }
 
+  removePerson(){
+    this.persons.splice(0,1);
+  }
+
   updateTwitter( event ){
     this.twitter = event.target.value;
+  }
+
+  addPerson(){
+    // let name = prompt('Digite el nombre');
+    // let age = prompt('Digite la edad');
+    let newPerson = new Person('nicolas', 22);
+    this.persons.push(newPerson);
   }
 
 }
